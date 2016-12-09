57b2941075ec1c9c1bfb2f163de7fc9756046520
Kevin Lin <developer@kevinlin.info>
2016-12-08 23:30:24 -0600

    test-authenticate.js

---
+++ test/server/api/test-authenticate.js
@@ -0,0 +1,26 @@
+import request from 'request';
+import sinon from 'sinon';
+import test from 'tape';
+
+import authenticate from '../../../src/server/api/authenticate';
+
+test('Authentication request of correct shape', (t) => {
+  const requestStub = sinon.stub(request, 'post', (opts, cb) => {
+    t.equal(opts.url, 'https://auth.kevinlin.info/auth-login', 'Correct authentication endpoint');
+    t.deepEqual(opts.form, {
+      /* eslint-disable camelcase */
+      httpd_username: 'username',
+      httpd_password: 'password'
+      /* eslint-enable camelcase */
+    }, 'Form passed to endpoint is correct');
+
+    cb();
+  });
+
+  authenticate('username', 'password', () => {
+    t.ok(requestStub.called, 'Network request is made');
+    request.post.restore();
+
+    t.end();
+  });
+});
