7c2a5c5436f892bd93eb6ec4d15597ae49e85746
Kevin Lin <developer@kevinlin.info>
2016-12-14 13:39:42 -0600

    Install step in CI script

--- test/ci.sh
+++ test/ci.sh
@@ -1,6 +1,8 @@
 set -ex
 
-cp config/secrets.js.template config/secrets.jsa
+cp config/secrets.js.template config/secrets.js
+gem install sass
+npm install
 npm run lint
 npm run build
 npm run cover
