f714368adf75fe1b865957e36bb582bca4862f76
Kevin Lin <developer@kevinlin.info>
2016-12-14 19:10:50 -0800

    test-overlay

--- src/shared/components/layout/overlay.js
+++ src/shared/components/layout/overlay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Overlay = ({opacity, children}) => (
-  <div style={{
+  <div className="overlay-container" style={{
     opacity: opacity,
     transition: 'all 0.3s ease'
   }}>
