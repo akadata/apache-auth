e7627fa2dba97d8f9c810cc3ab01e73b9f369397
Kevin Lin <developer@kevinlin.info>
2016-12-28 09:33:28 -0800

    Experimental: notify deployment success with Allu

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -14,3 +14,5 @@ npm run build
 pm2 stop apache-auth || :
 pm2 start index.js --name apache-auth
 pm2 show apache-auth
+
+curl -X POST https://allu.internal.kevinlin.info/api/notify/text -d '{"tag": "Jenkins", "messageText": "Successfully deployed apache-auth ('"$(git branch | cut -c 3-)"', '"$(git sha)"')."}'
