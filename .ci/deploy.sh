3edc9f3478153721231d7aa8bc636fa0b9972392
Kevin Lin <developer@kevinlin.info>
2016-12-30 20:16:05 -0800

    Use allu CLI client for post-deploy success notification

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -15,4 +15,8 @@ pm2 stop apache-auth || :
 pm2 start index.js --name apache-auth
 pm2 show apache-auth
 
-curl -X POST https://allu.internal.kevinlin.info/api/notify/text -d '{"tag": "Jenkins", "messageText": "Successfully deployed apache-auth ('"$(git branch | cut -c 3-)"', '"$(git sha)"')."}'
+allu \
+    --skip-auth \
+    --type text \
+    --tag Jenkins \
+    --message "Successfully deployed apache-auth ("$(git rev-parse --abbrev-ref HEAD)", "$(git sha)")."
